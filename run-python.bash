7b24dfacd17c7e26d35c8f646771f0627c6500cd
Serghei Filippov <blastowind@gmail.com>
2016-10-05 22:49:27 -0400

    Adds log on startup.

--- run-python.bash
+++ run-python.bash
@@ -11,7 +11,11 @@ if [[ $1 =~ "--flush" ]]; then
 	exit 0
 fi
 
+# Get the date today
+TODAY=$(date --iso-8601="seconds")
+
 # Writes to a merged logfile, and separated logfiles
 # Also writes to stdout/stderr
 echo "LOGS: ${ALL_LOGS}"
+echo "[${TODAY})] Starting WSGI"
 PYTHONDONTWRITEBYTECODE=1 python bot.py >> ${MERGED_LOG} 2>&1
